2f4c1311ac48f1f53405a8499a5d064d78edee1b
Joshua Coales <joshua@coales.co.uk>
2020-04-09 02:12:11 +0100

    Handling multi-dream nights, moving context, feelings, and ratings out of dream-specific data

--- importers/import_dreams.py
+++ importers/import_dreams.py
@@ -1,4 +1,5 @@
 import json
+import re
 from datetime import date
 
 start_date = date(2016, 4, 12)
@@ -16,30 +17,26 @@ for line in spreadsheet_data:
         dream_data = {"dreams": []}
         continue
     if cells[0].startswith("1)"):
-        print("Multi dream")  # TODO
-        continue
-    if cells[0].lower().startswith("{dream "):
-        print("Multi dream 2")  # TODO
-        continue
-    dream_data = {
-        "dreams": [
-            {
-                "text": cells[0]
-            }
-        ]
-    }
+        dreams = [{"text": text} for text in filter(None, re.split(r" *\d+\) *", cells[0]))]
+    elif cells[0].lower().startswith("{dream "):
+        dreams = [{"text": text} for text in filter(None, re.split(r" *\{dream ?\d+\} *", cells[0], flags=re.I))]
+    elif cells[0].lower().startswith("{1"):
+        dreams = [{"text": text} for text in filter(None, re.split(r" *\{\d+\} *", cells[0]))]
+    else:
+        dreams = [{"text": cells[0]}]
+    dream_data = {"dreams": dreams}
     if cells[1] != "not in use":
         if cells[1] == "unknown":
-            dream_data["dreams"][0]["context"] = None
+            dream_data["context"] = None
         else:
-            dream_data["dreams"][0]["context"] = cells[1]
+            dream_data["context"] = cells[1]
     if cells[2] != "not in use":
         if cells[1] == "unknown":
-            dream_data["dreams"][0]["feeling"] = None
+            dream_data["feeling"] = None
         else:
-            dream_data["dreams"][0]["feeling"] = cells[2]
+            dream_data["feeling"] = cells[2]
     if cells[3] not in ["not in use", "none"]:
-        dream_data["dreams"][0]["disorientation"] = int(cells[3])
+        dream_data["disorientation"] = int(cells[3])
     if cells[4] not in ["not in use", "none"]:
-        dream_data["dreams"][0]["lewdness"] = int(cells[4])
+        dream_data["lewdness"] = int(cells[4])
     print(json.dumps(dream_data, indent=2))
