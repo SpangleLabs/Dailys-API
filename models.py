c3a1d0cd4fd57ccb4f1e3d4270860cf6c4f41e30
Joshua Coales <joshua@coales.co.uk>
2020-04-12 15:27:32 +0100

    Adding max disorientation and lewdness to dream view

--- models.py
+++ models.py
@@ -184,6 +184,8 @@ class Dream:
 
     def __init__(self, data):
         self.text = data["text"]
+        self.disorientation = data.get("disorientation")
+        self.lewdness = data.get("lewdness")
 
 
 class DreamNight(Data):
@@ -191,6 +193,7 @@ class DreamNight(Data):
     def __init__(self, json_data):
         super().__init__(json_data)
         self.dreams = [Dream(x) for x in json_data["data"]["dreams"]]
+        self.raw_data = json_data["data"]
 
     def dream_preview(self, length=50):
         if len(self.dreams) == 0:
@@ -207,3 +210,17 @@ class DreamNight(Data):
     @property
     def total_dreams_length(self):
         return sum(len(dream.text) for dream in self.dreams)
+
+    @property
+    def max_disorientation(self):
+        dream_values = [dream.disorientation for dream in self.dreams]
+        if list(filter(None, dream_values)):
+            return max(filter(None, dream_values))
+        return "-"
+
+    @property
+    def max_lewdness(self):
+        dream_values = [dream.lewdness for dream in self.dreams]
+        if list(filter(None, dream_values)):
+            return max(filter(None, dream_values))
+        return "-"
