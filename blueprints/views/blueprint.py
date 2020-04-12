57f22fba762bc7de380eae0929b1d333ecb3b441
Joshua Coales <joshua@coales.co.uk>
2020-04-12 01:42:52 +0100

    Pulling out remaining views from ViewsBlueprint

--- blueprints/views/blueprint.py
+++ blueprints/views/blueprint.py
@@ -1,23 +1,18 @@
 import json
-import math
 from typing import Dict
 
-import dateutil
 import flask
-from datetime import datetime, date, time
+from datetime import datetime
 
-import isodate
-from dateutil.relativedelta import relativedelta
-
+from blueprints.views.chores_board import ChoresBoardJsonView, ChoresBoardView
+from blueprints.views.dreams import DreamsRangeView, DreamsView
 from blueprints.views.fa_notifications import FANotificationsRangeView, FANotificationsView
 from blueprints.views.mood import MoodRangeView, MoodView
 from blueprints.views.mood_weekly import MoodWeeklyRangeView, MoodWeeklyView
 from blueprints.views.sleep_status import SleepStatusJsonView, SleepStatusView
 from blueprints.views.sleep_time import SleepTimeRangeView, SleepTimeView
-from colour_scale import ColourScale
 from data_source import DataSource
 from decorators import view_auth_required
-from models import Chore, DreamNight
 from blueprints.base_blueprint import BaseBlueprint
 
 
@@ -38,7 +33,11 @@ class ViewsBlueprint(BaseBlueprint):
             MoodWeeklyRangeView(self.data_source),
             MoodWeeklyView(self.data_source),
             SleepStatusJsonView(self.data_source, self.config),
-            SleepStatusView(self.data_source, self.config)
+            SleepStatusView(self.data_source, self.config),
+            ChoresBoardJsonView(self.data_source),
+            ChoresBoardView(self.data_source),
+            DreamsRangeView(self.data_source),
+            DreamsView(self.data_source)
         ]
 
     def register(self):
@@ -46,10 +45,6 @@ class ViewsBlueprint(BaseBlueprint):
         for view in self._list_views():
             self.blueprint.add_url_rule(view.get_path(), f"{view.__class__.__name__}_call", view.call)
         self.blueprint.route("/named_dates/")(self.view_named_dates)
-        self.blueprint.route("/chores_board.json")(self.view_chores_board_json)
-        self.blueprint.route("/chores_board/")(self.view_chores_board)
-        self.blueprint.route("/dreams/<start_date:start_date>/<end_date:end_date>")(self.view_dreams_over_range)
-        self.blueprint.route("/dreams/")(self.view_dreams)
 
     @view_auth_required
     def list_views(self):
@@ -62,113 +57,3 @@ class ViewsBlueprint(BaseBlueprint):
             data = json.load(f)
             named_dates = {k: datetime.strptime(v, '%Y-%m-%d').date() for k, v in data.items()}
         return flask.render_template("named_dates.html", dates=named_dates)
-
-    @view_auth_required
-    def view_chores_board_json(self):
-        today = date.today()
-        chores_static = self.data_source.get_entries_for_stat_on_date("chores", "static")[0]
-        chores_data = self.data_source.get_entries_for_stat_over_range("chores", "earliest", "latest")
-        chores = [Chore(x) for x in chores_static['data']['chores']]
-        for chore_date in chores_data:
-            for chore in chores:
-                chore.parse_date_entry(chore_date)
-        # Sort chores into categories
-        categorised_chores = dict()
-        for chore in chores:
-            if chore.category not in categorised_chores:
-                categorised_chores[chore.category] = []
-            categorised_chores[chore.category].append(chore)
-        # Get layout info
-        layout = chores_static['data']['layout']
-        # Return json
-        return flask.jsonify({
-            "today": isodate.date_isoformat(today),
-            "chores": {k: [x.to_json() for x in v] for k, v in categorised_chores.items()},
-            "layout": layout
-        })
-
-    @view_auth_required
-    def view_chores_board(self):
-        chores_board = self.view_chores_board_json().get_json()
-        today = isodate.parse_date(chores_board['today'])
-        categorised_chores = {k: [Chore.from_complete_json(x) for x in v] for k, v in chores_board['chores'].items()}
-        layout = chores_board['layout']
-        # Calculate overdue and neglected chores
-        overdue_chores = []
-        neglected_chores = []
-        for chore_list in categorised_chores.values():
-            for chore in chore_list:
-                if chore.recommended_period is not None:
-                    if chore.is_overdue():
-                        overdue_chores.append(chore)
-                else:
-                    neglected_chores.append(chore)
-        # Sort overdue and neglected chores lists
-        overdue_chores.sort(key=lambda x: x.days_overdue(), reverse=True)
-        neglected_chores.sort(key=lambda x: x.days_since_done() or math.inf, reverse=True)
-        # Colour scales for non-recommended-period chores
-        start_colouring = today - isodate.parse_duration("P2M")
-        end_colouring = today - isodate.parse_duration("P1W")
-        colour_scale = ColourScale(
-            start_colouring, end_colouring,
-            ColourScale.RED, ColourScale.WHITE
-        )
-        # Render
-        return flask.render_template(
-            "chores_board.html",
-            today=today,
-            categorised_chores=categorised_chores,
-            layout=layout,
-            overdue_chores=overdue_chores,
-            neglected_chores=neglected_chores,
-            colour_scale=colour_scale,
-        )
-
-    @view_auth_required
-    def view_dreams_over_range(self, start_date, end_date):
-        dreams_data = self.data_source.get_entries_for_stat_over_range("dreams", start_date, end_date)
-        dream_nights = [DreamNight(x) for x in dreams_data]
-        static_data = self.data_source.get_entries_for_stat_on_date("dreams", "static")
-        # Fill in missing dates
-        if static_data and "all_nights_start" in static_data[0]["data"]:
-            dream_dates = [night.date.date() for night in dream_nights]
-            start_date = dateutil.parser.parse(static_data[0]["data"]["all_nights_start"]).date()
-            end_date = max(dream_dates)
-            current_date = max(start_date, min(dream_dates))
-            while current_date <= end_date:
-                if current_date not in dream_dates:
-                    dream_nights.append(DreamNight({"date": datetime.combine(current_date, time(0, 0, 0)), "source": "Auto-generated", "stat_name": "dreams", "data": {"dreams": []}}))
-                current_date += relativedelta(days=1)
-            dream_nights.sort(key=lambda x: x.date.date())
-        # Stats
-        count_with_dreams = len([night for night in dream_nights if night.dream_count > 0])
-        count_without_dreams = len(dream_nights) - count_with_dreams
-        percentage_with_dreams = f"{100*(count_with_dreams / len(dream_nights)):.2f}%"
-        max_dreams = max([night.dream_count for night in dream_nights])
-        max_length = max([night.total_dreams_length for night in dream_nights])
-        # Scales
-        dream_count_scale = ColourScale(
-            0, max(len(night.dreams) for night in dream_nights),
-            ColourScale.WHITE, ColourScale.RED
-        )
-        dream_length_scale = ColourScale(
-            0, max(night.total_dreams_length for night in dream_nights),
-            ColourScale.WHITE, ColourScale.RED
-        )
-        return flask.render_template(
-            "dreams.html",
-            dream_nights=dream_nights,
-            count_with_dreams=count_with_dreams,
-            count_without_dreams=count_without_dreams,
-            percentage_with_dreams=percentage_with_dreams,
-            max_dreams=max_dreams,
-            max_length=max_length,
-            dream_count_scale=dream_count_scale,
-            dream_length_scale=dream_length_scale
-        )
-
-    @view_auth_required
-    def view_dreams(self):
-        return self.view_dreams_over_range("earliest", "latest")
-
-
---
+++ blueprints/views/dreams.py
@@ -0,0 +1,66 @@
+from datetime import datetime, time
+
+import dateutil
+import flask
+from dateutil.relativedelta import relativedelta
+
+from blueprints.views.base_view import View
+from colour_scale import ColourScale
+from models import DreamNight
+
+
+class DreamsRangeView(View):
+    def get_path(self):
+        return "/dreams/<start_date:start_date>/<end_date:end_date>"
+
+    def call(self, **kwargs):
+        start_date = kwargs["start_date"]
+        end_date = kwargs["end_date"]
+        dreams_data = self.data_source.get_entries_for_stat_over_range("dreams", start_date, end_date)
+        dream_nights = [DreamNight(x) for x in dreams_data]
+        static_data = self.data_source.get_entries_for_stat_on_date("dreams", "static")
+        # Fill in missing dates
+        if static_data and "all_nights_start" in static_data[0]["data"]:
+            dream_dates = [night.date.date() for night in dream_nights]
+            start_date = dateutil.parser.parse(static_data[0]["data"]["all_nights_start"]).date()
+            end_date = max(dream_dates)
+            current_date = max(start_date, min(dream_dates))
+            while current_date <= end_date:
+                if current_date not in dream_dates:
+                    dream_nights.append(DreamNight({"date": datetime.combine(current_date, time(0, 0, 0)), "source": "Auto-generated", "stat_name": "dreams", "data": {"dreams": []}}))
+                current_date += relativedelta(days=1)
+            dream_nights.sort(key=lambda x: x.date.date())
+        # Stats
+        count_with_dreams = len([night for night in dream_nights if night.dream_count > 0])
+        count_without_dreams = len(dream_nights) - count_with_dreams
+        percentage_with_dreams = f"{100*(count_with_dreams / len(dream_nights)):.2f}%"
+        max_dreams = max([night.dream_count for night in dream_nights])
+        max_length = max([night.total_dreams_length for night in dream_nights])
+        # Scales
+        dream_count_scale = ColourScale(
+            0, max(len(night.dreams) for night in dream_nights),
+            ColourScale.WHITE, ColourScale.RED
+        )
+        dream_length_scale = ColourScale(
+            0, max(night.total_dreams_length for night in dream_nights),
+            ColourScale.WHITE, ColourScale.RED
+        )
+        return flask.render_template(
+            "dreams.html",
+            dream_nights=dream_nights,
+            count_with_dreams=count_with_dreams,
+            count_without_dreams=count_without_dreams,
+            percentage_with_dreams=percentage_with_dreams,
+            max_dreams=max_dreams,
+            max_length=max_length,
+            dream_count_scale=dream_count_scale,
+            dream_length_scale=dream_length_scale
+        )
+
+
+class DreamsView(DreamsRangeView):
+    def get_path(self):
+        return "/dreams/"
+
+    def call(self, **kwargs):
+        return super().call(start_date="earliest", end_date="latest")
