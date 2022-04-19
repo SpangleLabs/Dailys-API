1cb59b1dab30788563b9fefa8babfbb4163065cb
Joshua Coales <joshua@coales.co.uk>
2022-04-19 22:40:28 +0100

    Fixing named dates listing view

--- dailys_web/blueprints/views/named_dates.py
+++ dailys_web/blueprints/views/named_dates.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import flask
 
 from dailys_web.blueprints.views.base_view import View
+from dailys_web.path_converters import NAMED_DATES
 
 
 class NamedDatesView(View):
@@ -11,7 +12,4 @@ class NamedDatesView(View):
         return "/named_dates/"
 
     def call(self, **kwargs):
-        with open("named-dates.json", "r") as f:
-            data = json.load(f)
-            named_dates = {k: datetime.strptime(v, '%Y-%m-%d').date() for k, v in data.items()}
-        return flask.render_template("named_dates.html", dates=named_dates)
+        return flask.render_template("named_dates.html", dates=NAMED_DATES)
