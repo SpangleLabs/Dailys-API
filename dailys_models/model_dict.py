cb19bff472ca74c8b2438b3b8b2d6e20a39888f1
JC <joshua@coales.co.uk>
2022-04-21 14:57:24 +0100

    Adding MoodDay to MODEL_DICT, and reordering entries

--- dailys_models/model_dict.py
+++ dailys_models/model_dict.py
@@ -1,14 +1,16 @@
 from dailys_models.dream_night import DreamNight
 from dailys_models.fa_data import FuraffinityData
-from dailys_models.sleep_data import SleepData
+from dailys_models.mood_measurement import MoodDay
 from dailys_models.questions import QuestionsDay
+from dailys_models.sleep_data import SleepData
 
 MODEL_DICT = {
-    "sleep": SleepData,
-    "duolingo": None,
     "chores": None,
-    "furaffinity": FuraffinityData,
     "dreams": DreamNight,
-    "mood": None,
-    "questions": QuestionsDay
+    "duolingo": None,
+    "furaffinity": FuraffinityData,
+    "mood": MoodDay,
+    "questions": QuestionsDay,
+    "sleep": SleepData,
+    "sploo": None
 }
