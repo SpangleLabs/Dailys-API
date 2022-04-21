54ef69cdaaeb1d4a1b293846f9bc11c7473c58c5
JC <joshua@coales.co.uk>
2022-04-21 12:29:40 +0100

    Adding questions data to MODEL_DICT

--- dailys_models/model_dict.py
+++ dailys_models/model_dict.py
@@ -1,6 +1,7 @@
 from dailys_models.dream_night import DreamNight
 from dailys_models.fa_data import FuraffinityData
 from dailys_models.sleep_data import SleepData
+from dailys_models.questions import QuestionsDay
 
 MODEL_DICT = {
     "sleep": SleepData,
@@ -8,5 +9,6 @@ MODEL_DICT = {
     "chores": None,
     "furaffinity": FuraffinityData,
     "dreams": DreamNight,
-    "mood": None
+    "mood": None,
+    "questions": QuestionsDay
 }
